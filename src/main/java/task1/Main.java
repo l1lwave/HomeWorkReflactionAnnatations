6565996b79bf30451f73ece243a55a24c920e3a3
Putintsev_Kirill <135183716+l1lwave@users.noreply.github.com>
2025-01-29 00:15:57 +0200

    Update Main.java

--- src/main/java/task1/Main.java
+++ src/main/java/task1/Main.java
@@ -1,4 +1,27 @@
 package task1;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class Main {
+    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
+        SumNumbers sumNumbers = new SumNumbers();
+        Class<?> cls = sumNumbers.getClass();
+
+        Method[] methods = cls.getDeclaredMethods();
+        for (Method method : methods) {
+            if(method.isAnnotationPresent(MyTest.class)) {
+                MyTest myTest = method.getAnnotation(MyTest.class);
+                int result = (Integer)method.invoke(sumNumbers, myTest.a(), myTest.b());
+                System.out.println(result);
+            }
+        }
+    }
+}
+
+class SumNumbers {
+    @MyTest(a = 15, b = 23)
+    public static int sumNumbers(int n, int k) {
+        return n + k;
+    }
 }
