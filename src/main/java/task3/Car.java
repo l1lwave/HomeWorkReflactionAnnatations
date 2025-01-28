11074c57ed710ce32d93702ad130a06b7b5481ba
Putintsev_Kirill <135183716+l1lwave@users.noreply.github.com>
2025-01-29 00:17:40 +0200

    Update Car.java

--- src/main/java/task3/Car.java
+++ src/main/java/task3/Car.java
@@ -1,4 +1,24 @@
 package task3;
 
 public class Car {
+    @Save
+    private String color;
+    @Save
+    private int speed;
+    private String serialNumber;
+
+    public Car(String color, int speed, String serialNumber) {
+        this.color = color;
+        this.speed = speed;
+        this.serialNumber = serialNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "color='" + color + '\'' +
+                ", speed=" + speed +
+                ", serialNumber='" + serialNumber + '\'' +
+                '}';
+    }
 }
