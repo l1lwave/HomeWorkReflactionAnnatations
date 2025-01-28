136103cf36fd4481b52d046401d820c65899f9f5
Putintsev_Kirill <135183716+l1lwave@users.noreply.github.com>
2025-01-29 00:17:23 +0200

    TextContainer.java

--- src/main/java/task2/TextContainer.java
+++ src/main/java/task2/TextContainer.java
@@ -1,4 +1,30 @@
 package task2;
 
-public class TextContainer {
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@interface SaveTo {
+    String path();
+}
+
+@Retention(RetentionPolicy.RUNTIME)
+@interface Saver {}
+
+
+@SaveTo(path = "file.txt")
+class TextContainer {
+    String text = "Hello I love Java";
+
+    @Saver
+    public void saveText(String path) throws IOException {
+        FileWriter writer = new FileWriter(path);
+        try {
+            writer.write(text);
+        } finally {
+            writer.close();
+        }
+
+    }
 }
